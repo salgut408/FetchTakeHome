9fe59e42795648332c13515948f0c3eeac732a32
salgut408 <sgutie@saic.edu>
2022-08-26 12:49:03 -0500

    Sorted with item Comparator now

--- app/src/main/java/com/example/android/fetchtakehomeapp/util/ItemComparator.kt
+++ app/src/main/java/com/example/android/fetchtakehomeapp/util/ItemComparator.kt
@@ -8,7 +8,7 @@ class ItemComparator {
         override fun compare(a: JsonResponseModel?, b: JsonResponseModel?): Int = when {
             a?.listId != b?.listId -> a?.listId!! - b?.listId!!
             a?.id != b?.id -> a?.id!! - b?.id!!
-            else -> 6
+            else -> 0
         }
     }
 }
